4ab54f453cf9fe60abe82a4081465cfadde23efd
Alison Lee <alison2686@gmail.com>
2023-01-10 14:01:10 -0800

    import screens App.js

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import AccountScreen from "./src/screens/AccountScreen";
+import CreatePinScreen from "./src/screens/CreatePinScreen";
+import FindRouteScreen from "./src/screens/FindRouteScreen";
+import PinConfirmationScreen from "./src/screens/PinConfirmationScreen";
+import RouteResultScreen from "./src/screens/RouteResultScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import UploadPhotoScreen from "./src/screens/UploadPhotoScreen";
 
 export default function App() {
   return (
