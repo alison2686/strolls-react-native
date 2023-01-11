c45b2369486af7b1196533096f8959cb6e0860a3
Alison Lee <65731037+alison2686@users.noreply.github.com>
2023-01-10 17:06:06 -0800

    add Bottom Tab Navigation (#1)

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -5,8 +5,8 @@ const AccountScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={{ fontSize: 40 }}>Account Screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} /> */}
     </View>
   );
 };
